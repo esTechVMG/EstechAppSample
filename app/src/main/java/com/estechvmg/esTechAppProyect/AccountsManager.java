fbc74d58fe1fcf1cd66bb5e0b01503a12d3d75fd
esTechVMG <vicente.maroto@escuelaestech.es>
2020-03-26 15:27:40 +0100

    Login Implemented

---
+++ app/src/main/java/com/estechvmg/esTechAppProyect/AccountsManager.java
@@ -0,0 +1,82 @@
+package com.estechvmg.esTechAppProyect;
+
+class VMGAccountManager{
+    private VMGAccount[] accs;
+    VMGAccountManager(){
+        accs=new VMGAccount[0];
+    }
+    VMGAccountManager(VMGAccount[] usersList){
+        accs=usersList;
+    }
+    public int getAccountsListSize(){
+        return accs.length;
+    }
+    public boolean createAccount(String user,String pass){
+        if(!searchDuplicates(user,pass)){
+            addAccount(user,pass);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean checkLoginCredentials(String user,String pass){
+        return searchDuplicates(user,pass);
+    }
+    private void addAccount(String user,String pass){
+        increaseListSize();
+        accs[accs.length-1]=new VMGAccount(user,pass);
+    }
+    private boolean searchDuplicates(String user,String pass){
+        for(int a=0;a!=accs.length;a++){
+            if(accs[a].isThisAccount(user,pass)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private void increaseListSize(){
+        VMGAccount[] newAccs=new VMGAccount[accs.length+1];
+        for(int a=0;a!=accs.length;a++){
+            newAccs[a]=accs[a];
+        }
+        accs=newAccs;
+    }
+}
+class VMGAccount{
+    private String user,password;
+    VMGAccount(String user,String password){
+        this.user=user;
+        this.password=password;
+    }
+    public boolean isThisAccount(VMGAccount in){
+        return isThisAccount(in.user,in.password);
+    }
+    public boolean isThisAccount(String user,String password){
+        return checkUser(user) && checkPassword(password);
+    }
+    public boolean setPassword(String oldPassword,String newPassword){
+        if(checkPassword(oldPassword)){
+            password=newPassword;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public String getUser() {
+        return user;
+    }
+    private boolean checkPassword(String password){
+        return this.password.equals(password);
+    }
+    private boolean checkUser(String user){
+        return this.user.equals(user);
+    }
+    public boolean setUser(String password,String newUser) {
+        if(checkPassword(password)){
+            user=newUser;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/estechvmg/esTechAppProyect/MainActivity.java
+++ app/src/main/java/com/estechvmg/esTechAppProyect/MainActivity.java
@@ -2,13 +2,21 @@ package com.estechvmg.esTechAppProyect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public int phoneNumber=1234567890;
+    private VMGAccountManager accounts=new VMGAccountManager(new VMGAccount[]{
+            new VMGAccount("invitado","estech1234"),
+            new VMGAccount("alumno","alumno1234"),
+            new VMGAccount("profesor","profesor1234"),
+    });
 
     public Button forgotButton,loginButton,callButton;
     public TextInputLayout userInput,passInput;
@@ -19,21 +27,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         forgotButton=findViewById(R.id.forgotButton);
         loginButton=findViewById(R.id.loginButton);
         callButton=findViewById(R.id.callButton);
-
+        userInput=findViewById(R.id.userInputLayout);
+        passInput=findViewById(R.id.passInputLayout);
+        forgotButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+        callButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Log.d("EsTechApp","Touching Button");
         switch (v.getId()){
             case R.id.loginButton:
-
+                if(verifyCredentials(userInput.getEditText().getText().toString(),passInput.getEditText().getText().toString())){
+                    Log.d("esTechApp","Login credentials accepted");
+                }else{
+                    Log.d("esTechApp","Login credentials rejected");
+                }
                 break;
             case R.id.callButton:
-
+                Log.d("EsTechApp","Calling");
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(intent);
                 break;
             case R.id.forgotButton:
 
-                break;
         }
+    }
+    public boolean verifyCredentials(String user,String pass){
+        return accounts.checkLoginCredentials(user,pass);
     }
 }
