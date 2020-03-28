60e6b6d0f67ccc1a84c6076563e3e84699589cec
esTechVMG <vicente.maroto@escuelaestech.es>
2020-03-28 21:23:54 +0100

    menu layout colors fixed menu not showing

--- app/src/main/java/com/estechvmg/esTechAppProyect/SecondActivity.java
+++ app/src/main/java/com/estechvmg/esTechAppProyect/SecondActivity.java
@@ -1,15 +1,18 @@
 package com.estechvmg.esTechAppProyect;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 public class SecondActivity extends AppCompatActivity {
+    public BottomNavigationView bottomNav;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        bottomNav = findViewById(R.id.navigation);
     }
 }
