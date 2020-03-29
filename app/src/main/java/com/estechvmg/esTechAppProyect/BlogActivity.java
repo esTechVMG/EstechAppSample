50a8ba6fcd7c75a7f4ced5930119c7b60304917f
esTechVMG <vicente.maroto@escuelaestech.es>
2020-03-29 13:25:34 +0200

    Create Basic Blog Layout

---
+++ app/src/main/java/com/estechvmg/esTechAppProyect/BlogActivity.java
@@ -0,0 +1,14 @@
+package com.estechvmg.esTechAppProyect;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BlogActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
